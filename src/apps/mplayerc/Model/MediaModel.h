7669dd51072ca3c2c46a489ccf074f450eb82158
cjbw1234 <cjbw1234@localhost>
2010-12-20 17:10:43 +0800

    Add code for MediaModel class

--- src/apps/mplayerc/Model/MediaModel.h
+++ src/apps/mplayerc/Model/MediaModel.h
@@ -1,16 +1,17 @@
 #pragma once
 
-
 #include "MediaComm.h"
-#include "SourceModel.h
+#include "SourceModel.h"
 #include <sqlitepp/sqlitepp.hpp>
 
+// When search the media, use this to judge if the media should be searched
 typedef struct
 {
     int uniqueid;
     std::wstring filename;
 } MediaFindCondition;
 
+// An implement class to add, find and delete media data in the sqlite database
 class MediaModel :
     public SourceModel<MediaData, MediaFindCondition>
 {
@@ -18,8 +19,10 @@ public:
     MediaModel();
     ~MediaModel();
     
+public:
     int GetCount();
 
+    void Add(const MediaData& mdData);
     void Add(const MediaDatas& data);
 
     void FindOne(MediaData& data, const MediaFindCondition& condition);
@@ -30,4 +33,6 @@ public:
     void Delete(const MediaFindCondition& condition);
     void DeleteAll();
 
+private:
+  sqlitepp::session m_db;
 };
--- src/apps/mplayerc/Model/SourceModel.h
+++ src/apps/mplayerc/Model/SourceModel.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Source Model, an abstract class used for add, find and delete media data
 template<class TDATA, class TCONDITION>
 class SourceModel
 {
