9252425ae4c1c7bb0b71bbd65ee4321ec3837850
SoleoShao <SoleoShao@localhost>
2011-02-09 18:12:16 +0800

    Removed some redundant files and code; Add some notes on how to compile pHash under Win32

--- Thirdparty/pHash/samplerate.h
+++ Thirdparty/pHash/samplerate.h
@@ -35,158 +35,160 @@ extern "C" {
 #endif	/* __cplusplus */
 
 
-/* Opaque data type SRC_STATE. */
-typedef struct SRC_STATE_tag SRC_STATE ;
+    /* Opaque data type SRC_STATE. */
+    typedef struct SRC_STATE_tag SRC_STATE ;
 
-/* SRC_DATA is used to pass data to src_simple() and src_process(). */
-typedef struct
-{	float	*data_in, *data_out ;
+    /* SRC_DATA is used to pass data to src_simple() and src_process(). */
+    typedef struct
+    {
+        float	*data_in, *data_out ;
 
-	long	input_frames, output_frames ;
-	long	input_frames_used, output_frames_gen ;
+        long	input_frames, output_frames ;
+        long	input_frames_used, output_frames_gen ;
 
-	int		end_of_input ;
+        int		end_of_input ;
 
-	double	src_ratio ;
-} SRC_DATA ;
+        double	src_ratio ;
+    } SRC_DATA ;
 
-/* SRC_CB_DATA is used with callback based API. */
-typedef struct
-{	long	frames ;
-	float	*data_in ;
-} SRC_CB_DATA ;
+    /* SRC_CB_DATA is used with callback based API. */
+    typedef struct
+    {
+        long	frames ;
+        float	*data_in ;
+    } SRC_CB_DATA ;
 
-/*
-** User supplied callback function type for use with src_callback_new()
-** and src_callback_read(). First parameter is the same pointer that was
-** passed into src_callback_new(). Second parameter is pointer to a
-** pointer. The user supplied callback function must modify *data to
-** point to the start of the user supplied float array. The user supplied
-** function must return the number of frames that **data points to.
-*/
+    /*
+    ** User supplied callback function type for use with src_callback_new()
+    ** and src_callback_read(). First parameter is the same pointer that was
+    ** passed into src_callback_new(). Second parameter is pointer to a
+    ** pointer. The user supplied callback function must modify *data to
+    ** point to the start of the user supplied float array. The user supplied
+    ** function must return the number of frames that **data points to.
+    */
 
-typedef long (*src_callback_t) (void *cb_data, float **data) ;
+    typedef long (*src_callback_t) (void *cb_data, float **data) ;
 
-/*
-**	Standard initialisation function : return an anonymous pointer to the
-**	internal state of the converter. Choose a converter from the enums below.
-**	Error returned in *error.
-*/
+    /*
+    **	Standard initialisation function : return an anonymous pointer to the
+    **	internal state of the converter. Choose a converter from the enums below.
+    **	Error returned in *error.
+    */
 
-SRC_STATE* src_new (int converter_type, int channels, int *error) ;
+    SRC_STATE* src_new (int converter_type, int channels, int *error) ;
 
-/*
-**	Initilisation for callback based API : return an anonymous pointer to the
-**	internal state of the converter. Choose a converter from the enums below.
-**	The cb_data pointer can point to any data or be set to NULL. Whatever the
-**	value, when processing, user supplied function "func" gets called with
-**	cb_data as first parameter.
-*/
+    /*
+    **	Initilisation for callback based API : return an anonymous pointer to the
+    **	internal state of the converter. Choose a converter from the enums below.
+    **	The cb_data pointer can point to any data or be set to NULL. Whatever the
+    **	value, when processing, user supplied function "func" gets called with
+    **	cb_data as first parameter.
+    */
 
-SRC_STATE* src_callback_new (src_callback_t func, int converter_type, int channels,
-				int *error, void* cb_data) ;
+    SRC_STATE* src_callback_new (src_callback_t func, int converter_type, int channels,
+                                 int *error, void* cb_data) ;
 
-/*
-**	Cleanup all internal allocations.
-**	Always returns NULL.
-*/
+    /*
+    **	Cleanup all internal allocations.
+    **	Always returns NULL.
+    */
 
-SRC_STATE* src_delete (SRC_STATE *state) ;
+    SRC_STATE* src_delete (SRC_STATE *state) ;
 
-/*
-**	Standard processing function.
-**	Returns non zero on error.
-*/
+    /*
+    **	Standard processing function.
+    **	Returns non zero on error.
+    */
 
-int src_process (SRC_STATE *state, SRC_DATA *data) ;
+    int src_process (SRC_STATE *state, SRC_DATA *data) ;
 
-/*
-**	Callback based processing function. Read up to frames worth of data from
-**	the converter int *data and return frames read or -1 on error.
-*/
-long src_callback_read (SRC_STATE *state, double src_ratio, long frames, float *data) ;
+    /*
+    **	Callback based processing function. Read up to frames worth of data from
+    **	the converter int *data and return frames read or -1 on error.
+    */
+    long src_callback_read (SRC_STATE *state, double src_ratio, long frames, float *data) ;
 
-/*
-**	Simple interface for performing a single conversion from input buffer to
-**	output buffer at a fixed conversion ratio.
-**	Simple interface does not require initialisation as it can only operate on
-**	a single buffer worth of audio.
-*/
+    /*
+    **	Simple interface for performing a single conversion from input buffer to
+    **	output buffer at a fixed conversion ratio.
+    **	Simple interface does not require initialisation as it can only operate on
+    **	a single buffer worth of audio.
+    */
 
-int src_simple (SRC_DATA *data, int converter_type, int channels) ;
+    int src_simple (SRC_DATA *data, int converter_type, int channels) ;
 
-/*
-** This library contains a number of different sample rate converters,
-** numbered 0 through N.
-**
-** Return a string giving either a name or a more full description of each
-** sample rate converter or NULL if no sample rate converter exists for
-** the given value. The converters are sequentially numbered from 0 to N.
-*/
+    /*
+    ** This library contains a number of different sample rate converters,
+    ** numbered 0 through N.
+    **
+    ** Return a string giving either a name or a more full description of each
+    ** sample rate converter or NULL if no sample rate converter exists for
+    ** the given value. The converters are sequentially numbered from 0 to N.
+    */
 
-const char *src_get_name (int converter_type) ;
-const char *src_get_description (int converter_type) ;
-const char *src_get_version (void) ;
+    const char *src_get_name (int converter_type) ;
+    const char *src_get_description (int converter_type) ;
+    const char *src_get_version (void) ;
 
-/*
-**	Set a new SRC ratio. This allows step responses
-**	in the conversion ratio.
-**	Returns non zero on error.
-*/
+    /*
+    **	Set a new SRC ratio. This allows step responses
+    **	in the conversion ratio.
+    **	Returns non zero on error.
+    */
 
-int src_set_ratio (SRC_STATE *state, double new_ratio) ;
+    int src_set_ratio (SRC_STATE *state, double new_ratio) ;
 
-/*
-**	Reset the internal SRC state.
-**	Does not modify the quality settings.
-**	Does not free any memory allocations.
-**	Returns non zero on error.
-*/
+    /*
+    **	Reset the internal SRC state.
+    **	Does not modify the quality settings.
+    **	Does not free any memory allocations.
+    **	Returns non zero on error.
+    */
 
-int src_reset (SRC_STATE *state) ;
+    int src_reset (SRC_STATE *state) ;
 
-/*
-** Return TRUE if ratio is a valid conversion ratio, FALSE
-** otherwise.
-*/
+    /*
+    ** Return TRUE if ratio is a valid conversion ratio, FALSE
+    ** otherwise.
+    */
 
-int src_is_valid_ratio (double ratio) ;
+    int src_is_valid_ratio (double ratio) ;
 
-/*
-**	Return an error number.
-*/
+    /*
+    **	Return an error number.
+    */
 
-int src_error (SRC_STATE *state) ;
+    int src_error (SRC_STATE *state) ;
 
-/*
-**	Convert the error number into a string.
-*/
-const char* src_strerror (int error) ;
+    /*
+    **	Convert the error number into a string.
+    */
+    const char* src_strerror (int error) ;
 
-/*
-** The following enums can be used to set the interpolator type
-** using the function src_set_converter().
-*/
+    /*
+    ** The following enums can be used to set the interpolator type
+    ** using the function src_set_converter().
+    */
 
-enum
-{
-	SRC_SINC_BEST_QUALITY		= 0,
-	SRC_SINC_MEDIUM_QUALITY		= 1,
-	SRC_SINC_FASTEST			= 2,
-	SRC_ZERO_ORDER_HOLD			= 3,
-	SRC_LINEAR					= 4,
-} ;
+    enum
+    {
+        SRC_SINC_BEST_QUALITY		= 0,
+        SRC_SINC_MEDIUM_QUALITY		= 1,
+        SRC_SINC_FASTEST			= 2,
+        SRC_ZERO_ORDER_HOLD			= 3,
+        SRC_LINEAR					= 4,
+    } ;
 
-/*
-** Extra helper functions for converting from short to float and
-** back again.
-*/
+    /*
+    ** Extra helper functions for converting from short to float and
+    ** back again.
+    */
 
-void src_short_to_float_array (const short *in, float *out, int len) ;
-void src_float_to_short_array (const float *in, short *out, int len) ;
+    void src_short_to_float_array (const short *in, float *out, int len) ;
+    void src_float_to_short_array (const float *in, short *out, int len) ;
 
-void src_int_to_float_array (const int *in, float *out, int len) ;
-void src_float_to_int_array (const float *in, int *out, int len) ;
+    void src_int_to_float_array (const int *in, float *out, int len) ;
+    void src_float_to_int_array (const float *in, int *out, int len) ;
 
 
 #ifdef __cplusplus
