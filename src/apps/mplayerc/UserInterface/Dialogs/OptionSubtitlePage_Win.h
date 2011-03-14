e50ad71d3bd823eb9d13233b24585fe87e02216f
Tomasen <Tomasen@localhost>
2011-03-14 22:54:40 +0800

    Backed out changeset: fc41fd6dfa15

--- src/apps/mplayerc/UserInterface/Dialogs/OptionSubtitlePage_Win.h
+++ src/apps/mplayerc/UserInterface/Dialogs/OptionSubtitlePage_Win.h
@@ -2,7 +2,6 @@
 #define OPTIONSUBTITLEPAGE_WIN_H
 
 #include "../../resource.h"
-#include "SubtitleListBox.h"
 
 class OptionSubtitlePage:
   public WTL::CPropertyPageImpl<OptionSubtitlePage>,
@@ -16,10 +15,7 @@ public:
   BEGIN_MSG_MAP(OptionSubtitlePage)
     MSG_WM_INITDIALOG(OnInitDialog)
     MSG_WM_DESTROY(OnDestroy)
-    COMMAND_HANDLER_EX(IDC_CMB_SET_SUBTITLE, CBN_SELCHANGE, OnComboSetSubtitleChange)
     COMMAND_HANDLER_EX(IDC_LIST, LBN_SELCHANGE, OnSubtitleStyleChange)
-    COMMAND_HANDLER_EX(IDC_EDIT_SUBTITLE_FONTSIZE, EN_CHANGE, OnSubtitleSizeChange)
-    COMMAND_HANDLER_EX(IDC_BUTTON_SUBTITLE_FONTCOLOR, BN_CLICKED, OnSubtitleColorChange)
     COMMAND_HANDLER_EX(IDC_BUTTON_SAVESUBTITLE_CUSTOM_FOLDER, BN_CLICKED, OnBrowserForFolder)
     COMMAND_HANDLER_EX(IDC_RADIO_SAVESUBTITLE_SAME_FOLDER, BN_CLICKED, OnSelectSameFolder)
     COMMAND_HANDLER_EX(IDC_RADIO_SAVESUBTITLE_CUSTOM_FOLDER, BN_CLICKED, OnSelectCustomFolder)
@@ -42,35 +38,21 @@ public:
   void OnSelectSameFolder(UINT uNotifyCode, int nID, CWindow wndCtl);
   void OnSelectCustomFolder(UINT uNotifyCode, int nID, CWindow wndCtl);
 
-  void OnSubtitleSizeChange(UINT uNotifyCode, int nID, CWindow wndCtl);
-  void OnSubtitleColorChange(UINT uNotifyCode, int nID, CWindow wndCtl);
-
-  void OnComboSetSubtitleChange(UINT uNotifyCode, int nID, CWindow wndCtl);
-
   // owner-draw logic for subtitle styles
   void DrawItem(LPDRAWITEMSTRUCT lpdis);
   void MeasureItem(LPMEASUREITEMSTRUCT lpmis);
 
-  // apply/cancel handler
+  // activate/apply handler
+  int OnSetActive();
   int OnApply();
-  BOOL OnQueryCancel();
 
   void RefreshStyles();
 
 private:
-  WTL::CComboBox  m_cmbSetSubtitle;
   WTL::CComboBox  m_secsubtitlestyle;
-  WTL::CEdit      m_edtFontSize;
-  WTL::CButton    m_btnFontColor;
-  WTL::CUpDownCtrl m_spnFontSize;  // spin control
-  CSubtitleListBox  m_subtitlestyle;
+  WTL::CListBox   m_subtitlestyle;
 
   WTL::CString    m_sCustomPath;
-
-  double          m_dOldMainSize;
-  double          m_dOldSecondSize;
-  COLORREF        m_crOldMainColor;
-  COLORREF        m_crOldSecondColor;
 
   int m_mainstyle;
   int m_secstyle;
--- src/apps/mplayerc/UserInterface/Support/SubtitleStyle.h
+++ src/apps/mplayerc/UserInterface/Support/SubtitleStyle.h
@@ -11,7 +11,12 @@
 class SubtitleStyle
 {
 public:
+  typedef enum _FontName {
+    None, SimHei, SimSun, KaiTi
+  }FontName;
+
   typedef struct _STYLEPARAM{
+    FontName  _fontname;
     wchar_t   fontname[128];
     int fontsize;
     int fontcolor;      // color is 0x00bbggrr
@@ -25,6 +30,7 @@ public:
 
   static bool GetStyleParams(int index_main, int index_sec, STYLEPARAM** param_refout);
   static int GetStyleCount(bool secondary = false);
+  static int DetectFontType(std::wstring fontname);
 #ifdef _WINDOWS_
   static void Paint(HDC dc, RECT* rc, int index_main, int index_sec, bool selected = false);
 #endif
