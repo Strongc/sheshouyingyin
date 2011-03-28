9b13d52b0fc2b23595b0cfb1ac44cbf091a4db69
ymmuse <ymmuse@localhost>
2011-03-28 12:53:02 +0800

    add SPremote IPC & SPremote IPC send msg test program.

--- Prototype/SPRemote/cmdmap.h
+++ Prototype/SPRemote/cmdmap.h
@@ -6,10 +6,19 @@
 #define ADD_REMOTECMD(cmd, msgid) \
   remotecmd[cmd] = msgid;
 
+typedef struct {
+  std::string cmd;
+  UINT msgid;
+  time_t timestamp;
+} RemoteMsg;
+
+#define MAX_REMOTEMSG_10 10
+#define REMOTEMSG_CHANNELNAME "SPlayer_remote_msgchannle"
+
 typedef std::map<std::string, int> REMOTECMD;
 
 void GetRemoteCmdMap(REMOTECMD& remotecmd)
 {
-  ADD_REMOTECMD("play", 1);
-  ADD_REMOTECMD("stop", 2);
+  ADD_REMOTECMD("play", 887);
+  ADD_REMOTECMD("stop", 888);
 }
