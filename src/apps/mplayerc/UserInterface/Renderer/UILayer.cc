e3e8dfecc37fa0dde6feadbe2050cc8ac6fe5e46
zhoufei <zhoufei@localhost>
2011-04-12 13:25:51 +0800

    Add: media tips when mouse hovered on the items which belong to list.

--- src/apps/mplayerc/UserInterface/Renderer/UILayer.cc
+++ src/apps/mplayerc/UserInterface/Renderer/UILayer.cc
@@ -72,7 +72,7 @@ BOOL UILayer::SetDisplay(BOOL display)
 
 BOOL UILayer::DoPaint(WTL::CDC& dc)
 {
-  if (!m_display && !m_fixdisplay)
+  if (!m_display || !m_fixdisplay)
     return FALSE;
 
   WTL::CDC texturedc;
@@ -91,10 +91,13 @@ BOOL UILayer::DoPaint(WTL::CDC& dc)
   return TRUE;
 }
 
-BOOL UILayer::OnHittest(POINT pt, BOOL blbtndown)
+BOOL UILayer::OnHittest(POINT pt, BOOL blbtndown, int extent)
 {
+  if (!m_fixdisplay)
+    return FALSE;
+
   RECT hittestrc = {m_texturepos.x, m_texturepos.y, m_texturepos.x + m_bm.bmWidth,
-             m_texturepos.y + m_bm.bmHeight};
+             m_texturepos.y + m_bm.bmHeight + extent};
   BOOL bl = PtInRect(&hittestrc, pt);
   if (bl)
     SetDisplay(TRUE);
