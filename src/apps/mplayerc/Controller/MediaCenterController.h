d55ebf61c7903e5f23e7264147c335f8a9ecd9be
cjbw1234 <cjbw1234@localhost>
2011-06-08 11:42:43 +0800

    Fix a bug: the film name editor can't use IME(e.g:can't input Chinese)

---
+++ src/apps/mplayerc/Controller/MediaCenterController.h
@@ -0,0 +1,99 @@
+﻿#pragma once
+
+#include "LazyInstance.h"
+#include "../Model/MediaComm.h"
+#include "../Model/MediaModel.h"
+#include "../Model/MediaTreeModel.h"
+#include "MediaSpiderFolderTree.h"
+#include <map>
+#include "..\UserInterface\Renderer\BlockList.h"
+#include "LoadMediaDataFromDB.h"
+#include "..\UserInterface\Renderer\MCStatusBar.h"
+#include "CoverController.h"
+
+class MediaCenterController:
+  public LazyInstanceImpl<MediaCenterController>
+{
+public:
+  MediaCenterController();
+  ~MediaCenterController();
+
+public:
+  // Maintenance for MC folder
+  static bool IsMCFolderExist();
+  static void CreateMCFolder();
+  static std::wstring GetMCFolder();
+
+  static std::wstring GetCoverPath(const std::wstring &sFilePath);
+  static std::wstring GetMediaHash(const std::wstring &sFilePath);
+
+public:
+  // Gui control, should not for other use
+
+   void SetMCCover();
+
+   void DoPaint(HDC hdc, RECT rcClient);
+
+   void UpdateBlock(RECT rc);
+   void DelBlock(int i);
+
+   void SetFrame(HWND hwnd);
+
+   void Playback(std::wstring file);
+
+   BOOL GetPlaneState();
+
+   void SetPlaneState(BOOL bl);
+
+   BlockListView& GetBlockListView();
+   media_tree::model& GetMediaTree();
+   HWND GetFilmNameEdit();
+   CoverController& GetCoverDownload();
+   
+   HRGN CalculateUpdateRgn(WTL::CRect& rc);
+
+   void SetCover(BlockUnit* unit, std::wstring orgpath);
+   HFONT GetFilmTextFont();
+   void SetFilmTextFont(int height, const std::wstring &family);
+
+// slots
+public:
+  void HandlePlayback(const MediaData &md);
+
+  void HandleDelBlock(const BlockUnit *pBlock);
+
+public:
+  // Data control
+  void SpiderThreadStart();
+  void SpiderThreadStop();
+
+  void CoverThreadStart();
+  void CoverThreadStop();
+
+  void SaveTreeDataToDB();
+
+  void LoadMediaData(int direction, std::list<BlockUnit*>* list, int viewcapacity, 
+                     int listcapacity, int remain, int times = 1);
+  
+  HANDLE GetMediaDataThreadHandle();
+
+  BOOL LoadMediaDataAlive();
+  
+private:
+  // GUI
+  HWND m_hwnd;
+  BOOL m_planestate;
+  
+  BlockListView m_blocklist;
+  WTL::CBitmap m_mccover;
+  BITMAP  m_mccoverbm;
+  CoverController m_cover;
+  BOOL m_initiablocklist;
+  LoadMediaDataFromDB m_loaddata;
+  HFONT m_hFilmTextFont;
+  
+  // Data
+  MediaModel            m_model;
+  media_tree::model     m_treeModel;
+  MediaSpiderFolderTree m_spider;
+};
